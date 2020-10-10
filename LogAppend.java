b94083fe2443bbd5b2251f615155fc44d8ce2937
Txiv Yawg <71901079+txivyawg@users.noreply.github.com>
2020-10-10 09:52:32 -0700

    Create LogAppend.java

---
+++ LogAppend.java
@@ -0,0 +1,8 @@
+package com.marcobrador.cybersecurity.capstone;
+
+public class LogAppend {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from " + LogAppend.class.getSimpleName());
+    }
+}
